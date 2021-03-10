8bac15b4470b1f3f95566e63991deb6cde90a0a5
Thomas Lane <tlane@Thomass-MBP.lan>
2021-03-10 15:31:12 -0500

    Tried to make a merge list

--- address.py
+++ address.py
@@ -1,22 +1,31 @@
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
 
 
 def run():
     df = pd.read_excel('./wrendata.xlsx')
+    df2 = pd.read_excel('./wrenactivemembers.xls')
+
     df['Address'] = df['Address'].str.lower()
     df['City'] = df['City'].str.lower()
 
-    df.sort_values(by=["Last name", "Address", "City", "Email Address"], inplace=True)
+    df.sort_values(by=["Last name", "Address", "City",
+                       "Email Address"], inplace=True)
     df.drop_duplicates(subset=["Address", "City"], keep="last", inplace=True)
 
     df = df.sort_values(by=["Last name"])
     df['Address'] = df['Address'].str.title()
     df['City'] = df['City'].str.title()
-    print(df)
 
-    df.to_excel (r'/Volumes/parkerlanedev/WRENAddressDupesRemoved.xlsx', index=False, header=True)
+    df.to_excel(r'/Users/tlane/Desktop/filewithactivemembersincluded.xlsx',
+                index=False, header=True)
+
+    mergedData = pd.merge(df, df2, on=['Last name', 'Address'])
+
+    print(mergedData)
+    mergedData.to_excel(r'/Users/tlane/Desktop/activemembersmix.xlsx',
+                        index=False, header=True)
+
 
 if __name__ == '__main__':
     run()
-    
